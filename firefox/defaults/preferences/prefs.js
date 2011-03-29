59cc45904fdfc4feefccdac2a73b43f1692a5275
Intars <the.mobix@gmail.com>
2011-03-29 14:09:12 +0300

    Renamed keySharky -> keyMazony

--- firefox/defaults/preferences/prefs.js
+++ firefox/defaults/preferences/prefs.js
@@ -1,16 +1,16 @@
-pref('extensions.keysharky.play',       '{"modifiers":["control","alt","shift"],"key":"Z","keycode":"","enabled":true}');
-pref('extensions.keysharky.stop',       '{"modifiers":["control","alt","shift"],"key":"X","keycode":"","enabled":true}');
-pref('extensions.keysharky.previous',   '{"modifiers":["control","alt","shift"],"key":"A","keycode":"","enabled":true}');
-pref('extensions.keysharky.next',       '{"modifiers":["control","alt","shift"],"key":"D","keycode":"","enabled":true}');
+pref('extensions.keymazony.play',       '{"modifiers":["control","alt","shift"],"key":"Z","keycode":"","enabled":true}');
+pref('extensions.keymazony.stop',       '{"modifiers":["control","alt","shift"],"key":"X","keycode":"","enabled":true}');
+pref('extensions.keymazony.previous',   '{"modifiers":["control","alt","shift"],"key":"A","keycode":"","enabled":true}');
+pref('extensions.keymazony.next',       '{"modifiers":["control","alt","shift"],"key":"D","keycode":"","enabled":true}');
 
-pref('extensions.keysharky.favorite',   '{"modifiers":["control","alt"],"key":"S","keycode":"","enabled":true}');
-pref('extensions.keysharky.voteup',     '{"modifiers":["control","alt"],"key":"A","keycode":"","enabled":true}');
-pref('extensions.keysharky.votedown',   '{"modifiers":["control","alt"],"key":"Z","keycode":"","enabled":true}');
-pref('extensions.keysharky.voteclear',  '{"modifiers":["control","alt"],"key":"Q","keycode":"","enabled":true}');
+pref('extensions.keymazony.favorite',   '{"modifiers":["control","alt"],"key":"S","keycode":"","enabled":true}');
+pref('extensions.keymazony.voteup',     '{"modifiers":["control","alt"],"key":"A","keycode":"","enabled":true}');
+pref('extensions.keymazony.votedown',   '{"modifiers":["control","alt"],"key":"Z","keycode":"","enabled":true}');
+pref('extensions.keymazony.voteclear',  '{"modifiers":["control","alt"],"key":"Q","keycode":"","enabled":true}');
 
-pref('extensions.keysharky.mute',       '{"modifiers":["control","shift"],"key":"M","keycode":"","enabled":true}');
-pref('extensions.keysharky.volup',      '{"modifiers":["control","shift"],"key":">","keycode":"","enabled":true}');
-pref('extensions.keysharky.voldown',    '{"modifiers":["control","shift"],"key":"<","keycode":"","enabled":true}');
+pref('extensions.keymazony.mute',       '{"modifiers":["control","shift"],"key":"M","keycode":"","enabled":true}');
+pref('extensions.keymazony.volup',      '{"modifiers":["control","shift"],"key":">","keycode":"","enabled":true}');
+pref('extensions.keymazony.voldown',    '{"modifiers":["control","shift"],"key":"<","keycode":"","enabled":true}');
 
-pref('extensions.keysharky.server_port',        8800);
-pref('extensions.keysharky.server_autostart',   false);
+pref('extensions.keymazony.server_port',        8800);
+pref('extensions.keymazony.server_autostart',   false);
